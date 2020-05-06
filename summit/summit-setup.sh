8478d0b5e2e86f4190a54b7449c1ba28e5d981b3
Khaled Janania <kjanania@redhat.com>
2020-05-06 12:14:24 -0400

    Some changes to automatically create apimanager and operator

--- summit/summit-setup.sh
+++ summit/summit-setup.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
+set -x
 
 oc new-project 3scale-operator-default
 
 oc -n 3scale-operator-default create secret generic redhat-registry --from-file=.dockerconfigjson=$HOME/.docker/config.json --type=kubernetes.io/dockerconfigjson
 oc -n 3scale-operator-default secrets link default redhat-registry --for=pull
 
-for i in `ls deploy/crds/*3scale.hosted*_crd.yaml`; do oc create -f $i ; done
+for i in `ls deploy/crds/*_crd.yaml`; do oc create -f $i ; done
 
 oc -n 3scale-operator-default create -f deploy/service_account.yaml
 oc create -n 3scale-operator-default -f deploy/role.yaml
-oc create -n 3scale-operator-default-f deploy/role_binding.yaml
+oc create -n 3scale-operator-default -f deploy/role_binding.yaml
 
 oc -n 3scale-operator-default create -f deploy/operator.yaml
 oc create -f summit/api-manager.yaml
+
+# Recreate the storage as ReadWriteOnce since the cluster doesn't support ReadWriteMany
+oc delete -f summit/system-storage.yaml
+oc create -f summit/system-storage.yaml
